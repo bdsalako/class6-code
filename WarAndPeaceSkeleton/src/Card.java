d7b8d477215f361d3b251795ef0e50bab9639acb
bdsalako <bdsalako@gmail.com>
2015-02-27 23:49:06 -0500

    Update Card.java

--- WarAndPeaceSkeleton/src/Card.java
+++ WarAndPeaceSkeleton/src/Card.java
@@ -1,13 +1,55 @@
 
 public class Card {
 	
-	public String toString() {
-			//TODO: complete this method definition
+	private CardValue cardValue;
+	private CardSuit cardSuit;
+	
+	public String toString(){
+		return this.cardValue + " of " + this.cardSuit;
 	}
 	
+	public CardValue getValue()
+	{
+		return cardValue;
+	}
+	
+	public CardSuit getSuit()
+	{
+		return cardSuit;
+	}
+	
+	Card(CardSuit cardSuit, CardValue cardValue)
+	{
+		this.cardSuit = cardSuit;
+		this.cardValue = cardValue;
+	}
+	
+	public boolean Winner(Card c)
+	{	
+		
+		if(this.getValue().compareTo(c.getValue()) > 0)
+			return true;
+		
+		
+		if(this.getValue().compareTo(c.getValue()) < 0)
+			return false;
+			
+		if (this.getValue().compareTo(c.getValue()) == 0)
+			if(this.getSuit().compareTo(c.getSuit()) > 0)
+				return true;
+		
+		if (this.getValue().compareTo(c.getValue()) == 0)
+			if(this.getSuit().compareTo(c.getSuit()) < 0)
+				return false;
+		
+		if (this.getValue().compareTo(c.getValue()) == 0)
+			if(this.getSuit().compareTo(c.getSuit()) == 0)
+				System.out.print("Same card");
+					
+		
+		 return false;
+	}
 
-	public boolean winner(Card c) {
-			// TODO: complete this method definition
-	}
-	
 }
+
+
